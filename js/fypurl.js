e865305a2e76c32dc9a8ea12fcf2c0613cac4714
David Barral <david.barral@trabesoluciones.com>
2011-03-23 16:12:46 +0100

    Bugfix: empty friends list was rendered as "undefined" in the options panel

--- js/fypurl.js
+++ js/fypurl.js
@@ -1,14 +1,14 @@
 function restore_options() {
-   $("friends").value = localStorage["friends"];
+   $("friends").value = localStorage["friends"] || "";
  }
- 
+
 function save_options() {
 	localStorage["friends"] = $F("friends");
-} 
+}
 
 function render_friends() {
 	var friends = (localStorage["friends"] || "").split("\n").map(function(f) { return f.strip() }).reject(function(f) { return f.blank(); });
-	
+
 	if (friends.size() == 0) {
      	friend_fyps.hide();
 		return;
@@ -22,20 +22,21 @@ function render_friends() {
 		$('friend_fyps').appendChild(li);
 	});
 }
- 
+
 function go_to_fyp(event) {
    event.stop();
    chrome.tabs.create({url: "http://fypurl.com/" + this.rel });
 }
- 
+
 function fyp() {
 	chrome.tabs.getSelected(null, function(tab) {
 		window.open('http://fypurl.com/fyp_express?fyp_action=fyp_express&url='+encodeURIComponent(tab.url),'fyp_express_window','width=710,height=440,left=75,top=20,resizable=yes,scrollbars=yes');
 		wyp_express_window.focus();
 	});
  }
- 
+
 function unfyp() {
 	window.open('http://fypurl.com/unfyp_express?fyp_action=unfyp_express','fyp_express_window','width=710,height=440,left=75,top=20,resizable=yes,scrollbars=yes');
-    fyp_express_window.focus();      
+    fyp_express_window.focus();
 }
+
